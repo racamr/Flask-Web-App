c69eb70094cccfb1ef4b75933feea9e9de1b5a88
Rac Amr <rachelamruthaluri2@gmail.com>
2023-06-29 12:07:45 +0200

    Frontend edits

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ import stripe
 def home():
     return render_template('home.html', title='Home')
 
-
+# About
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About Us')
 
 # Jobs page in homepage
 @app.route('/jobs')
